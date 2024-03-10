23dce2b4b66cdb404bad57cd010efb77d1f61065
tomerkor1985 <tomerkor1985@gmail.com>
2024-03-10 11:53:20 +0200

    Finish mission

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# matrix_home_assignment
+# API Test Automation - Home Assignment
+*This repository contains the solution to the API test automation home assignment, which involves interacting with the Pokémon API using Python and the pytest framework.*
+
+Assignment Scenarios:
+1. Verify Pokémon Type API Response
+Use the resource https://pokeapi.co/api/v2/type.
+Verify that the Pokémon type API returns a response with type JSON.
+Verify that there are exactly 20 different Pokémon types.
+
+
+2. Find ID of “Fire Type” Pokémon
+Find the ID of the “Fire type” Pokémon.
+Validate that:
+Pokémon “charmander” is in the JSON of the Fire Pokémon list.
+Pokémon “bulbasaur” is not in the JSON of the Fire Pokémon list.
+
+
+3. Identify Five Heaviest Pokémon of the Fire Type
+Identify the five heaviest Pokémon of the Fire type.
+Ensure they have the expected weights:
+   - ‘charizard-gmax’: ‘weight’: 10000
+   - ‘cinderace-gmax’: ‘weight’: 10000
+   - ‘coalossal-gmax’: ‘weight’: 10000
+   - ‘centiskorch-gmax’: ‘weight’: 10000
+   - ‘groudon-primal’: ‘weight’: 9997
+
+
+Libraries Used:
+- requests: Used to interact with the Pokémon API.
+- pytest: Used for writing and executing test cases.
+
+Test Function Details:
+* test_pokemon_api.py - 
+*Contains test functions for the given scenarios.*
+
+## Installation:
+Please install requirements.txt file before using this project.
+`pip install -r requirements.txt`
+
+## Run tests
+1. You can run test by type in command line : `pytest -v`
+2. You can use IDE to run tests.
+
+### Questions
+* If you have any questions about the project feel free to contact me via Email: **tomerkor1985@gmail.com**
--- pokemon_utils.py
+++ pokemon_utils.py
@@ -10,50 +10,82 @@ class PokemonConnection:
         :param path: An addition to the original path. i.e.: 'https://pokeapi.co/api/v2/' + 'type'
         :return: Json response from selected path.
         """
-        return requests.get(self.url + path)
+        try:
+            response = requests.get(self.url + path)
+            return response
+        except requests.exceptions.RequestException as e:
+            print(f"Error connecting to API: {e}")
+            raise ConnectionError(f"API connection error: {e}")  # Raise specific exception
 
 
 class PokemonData(PokemonConnection):
 
     def get_poke_type_id(self, poke_type):
         """
+        This function is about to get the ID of Pokemons by type.
         :param poke_type: could be : fire, water, shadow, rock, steel etc...
         :return: the type ID.
         """
-        data = self.connect_to_api("type").json()['results']
-        type_id = None
-        for i in data:
-            if i['name'] != poke_type:
-                continue
-            type_id = i['url'].split('/')[-2]
-        return int(type_id)
+        try:
+            data = self.connect_to_api("type").json()['results']
+            for entry in data:
+                if entry.get('name') == poke_type:
+                    return int(entry['url'].split('/')[-2])
+            raise ValueError(f"Type '{poke_type}' not found.")
+        except (KeyError, ValueError) as e:
+            print(f"Error getting type ID: {e}")
+            raise ValueError(f"Error getting type ID: {e}")  # Raise specific exception
 
     def get_pokemon_list_by_type_id(self, poke_type):
-        # Get ID for API url.
-        id_for_url = self.get_poke_type_id(poke_type)
-        # Get pokemons list by API URL ID.
-        pokemons_list_type_id = self.connect_to_api(f"type/{id_for_url}").json()['pokemon']
-        # Get a list of all existing pokemons under selected type.
-        existing_pokemons = [p_name['pokemon']['name'] for p_name in pokemons_list_type_id]
-        return existing_pokemons
+        """
+        This function getting the pokemon list by the type ID.
+        :param poke_type: could be : fire, water, shadow, rock, steel etc...
+        :return: List of pokemons under the selected type.
+        """
+        try:
+            id_for_url = self.get_poke_type_id(poke_type)
+            pokemons_list_type_id = self.connect_to_api(f"type/{id_for_url}").json()['pokemon']
+            existing_pokemons = [p_name['pokemon']['name'] for p_name in pokemons_list_type_id]
+            return existing_pokemons
+        except Exception as e:
+            print(f"Error getting Pokemon list by type ID: {e}")
+            raise Exception(f"Error getting Pokemon list by type ID: {e}")  # Raise custom exception
 
     def get_pokemons_url_by_type(self, poke_type):
+        """
+        This function will get the pokemons url.
+        :param poke_type: could be : fire, water, shadow, rock, steel etc...
+        :return: list of Dicts with {pokemon_name: pokemon_url}
+        """
         # Get ID for API url.
         id_for_url = self.get_poke_type_id(poke_type)
-        # Get pokemons list by API URL ID.
-        pokemons_list_type_id = self.connect_to_api(f"type/{id_for_url}").json()['pokemon']
-        poke_urls = {i['pokemon']['name']: i['pokemon']['url'] for i in pokemons_list_type_id}
-        return poke_urls
+        # Ensure id_for_url is valid
+        if id_for_url is None:
+            raise ValueError(f"Invalid type '{poke_type}'")
+        try:
+            # Get pokemons list by API URL ID.
+            pokemons_list_type_id = self.connect_to_api(f"type/{id_for_url}").json()['pokemon']
+            poke_urls = {i['pokemon']['name']: i['pokemon']['url'] for i in pokemons_list_type_id}
+            return poke_urls
+        except (KeyError, ValueError) as e:
+            print(f"Error getting Pokemon URLs by type: {e}")
+            raise Exception(f"Error getting Pokemon URLs by type: {e}")  # Raise custom exception
 
     def get_poke_weight_by_name(self, poke_name, poke_type):
-        poke_url = None
-        poke_dict = self.get_pokemons_url_by_type(poke_type)
-        for poke in poke_dict:
-            if poke != poke_name:
-                continue
-            poke_url = poke_dict[poke]
-            break
-        if poke_url is None:
-            return False
-        return requests.get(poke_url).json()['weight']
-
+        """
+        This function will get the weight of specific pokemon by given name and type.
+        :param poke_name: Pokemon name i.e. : "charmander"
+        :param poke_type: could be : fire, water, shadow, rock, steel etc...
+        :return: Pokemon weight.
+        """
+        try:
+            poke_urls = self.get_pokemons_url_by_type(poke_type)
+            poke_url = poke_urls.get(poke_name)
+            if poke_url is None:
+                raise ValueError(f"Pokemon '{poke_name}' not found in type '{poke_type}'.")
+            response = requests.get(poke_url)
+            response.raise_for_status()  # Raise exception for non-200 status codes
+            return response.json().get('weight')
+        except Exception as e:
+            print(f"Error getting Pokemon weight by name: {e}")
+            raise Exception(f"Error getting Pokemon weight by name: {e}")
